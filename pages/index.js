c1ca07b0c5e572dad5fa6f71079f5e8805871d84
IJayatharan <indran@calcey.com>
2022-10-21 05:23:12 +0000

    Test1

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Test 1
         </h1>
 
         <p className={styles.description}>
